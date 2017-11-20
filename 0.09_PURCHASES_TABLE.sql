90545f33febdf3bfd50e3f0dbdfd10e11bb9f192
Mjmarek <monicamarek24@gmail.com>
2017-11-20 14:35:23 -0500

    Added foreign keys

---
+++ 0.09_PURCHASES_TABLE.sql
@@ -0,0 +1,22 @@
+﻿USE SQL_Library;    
+GO    
+
+CREATE TABLE dbo.Purchases (
+    PurchaseID INT IDENTITY (1, 1) NOT NULL,
+    ProductID  INT NOT NULL,
+    CustomerID INT NOT NULL,
+    Quantity   INT NULL,
+    CONSTRAINT [PK_dbo.Purchases] PRIMARY KEY CLUSTERED ([PurchaseID] ASC),
+    CONSTRAINT [FK_dbo.Purchases_dbo.Customer_CustomerID] FOREIGN KEY ([CustomerID]) 
+		REFERENCES [dbo].[Customers] ([CustomerID]) ON DELETE CASCADE,
+    CONSTRAINT [FK_dbo.Purchases_dbo.PRODUCTS_ProductID] FOREIGN KEY ([ProductID]) 
+		REFERENCES [dbo].[PRODUCTS] ([ProductID]) ON DELETE CASCADE
+);
+
+GO
+CREATE NONCLUSTERED INDEX [IX_ProductID]
+    ON [dbo].[Purchases]([ProductID] ASC);
+
+GO
+CREATE NONCLUSTERED INDEX [IX_CustomerID]
+    ON [dbo].[Purchases]([CustomerID] ASC);
